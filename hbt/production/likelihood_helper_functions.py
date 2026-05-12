Finalized cflow update. Added script to plot jacobian terms. Fixed bug in pdf_inputs / likelihood input creation for higgs inputs that messed up boosting into HH system for phi(H_bb) and cos_theta(H_bb), which resulted in wrong inputs and failing gradients

hbt/production/likelihood_helper_functions.py
@@ -151,6 +151,7 @@ def calculate_likelihood_ratio_for_plotting(
     bins_per_dim_2d: np.ndarray = np.array([27, 27]),
     bins_per_dim_3d: np.ndarray = np.array([20, 7, 7]),
     statistical_binning: bool = True,
+    allow_hist_creation: bool = False,
     do_constr: bool = True,
     do_jacobian: bool = False,
     bin_filling: bool = False,
@@ -184,11 +185,11 @@ def calculate_likelihood_ratio_for_plotting(
         eval_data_signal,
         n_bins_1d,
         bins_per_dim=bins_per_dim_2d,
-        allow_hist_creation=False,
-        statistical_binning=True,
-        do_constr=True,
-        bin_filling=False,
-        do_jacobian=True,
+        allow_hist_creation=allow_hist_creation,
+        statistical_binning=statistical_binning,
+        do_constr=do_constr,
+        bin_filling=bin_filling,
+        do_jacobian=do_jacobian,
     )
 
     eval_data_bg = hhf.get_data(eval_data_path, "pdf_input_vars_reco_top", drop_nones=False)
@@ -196,11 +197,11 @@ def calculate_likelihood_ratio_for_plotting(
         eval_data_bg,
         n_bins_1d,
         bins_per_dim=bins_per_dim_3d,
-        allow_hist_creation=False,
-        statistical_binning=True,
-        do_constr=True,
-        bin_filling=False,
-        do_jacobian=True,
+        allow_hist_creation=allow_hist_creation,
+        statistical_binning=statistical_binning,
+        do_constr=do_constr,
+        bin_filling=bin_filling,
+        do_jacobian=do_jacobian,
     )
 
     ev_idx_mask = ev_idx_higgs == ev_idx_top

hbt/production/pdf_input_vars_reco.py
@@ -312,6 +312,7 @@ def calculate_hbb(inputs):
     b2 = det_coords_to_fourmomentum(inputs[4:8])
     b1b2 = jax.numpy.concatenate([b1, b2], axis=0)
     h1 = four_vec_sum(b1b2)
+    # in 4-momentum space
     return h1
 
 
@@ -320,6 +321,7 @@ def calculate_htautau(inputs):
     tau2 = det_coords_to_fourmomentum(inputs[12:])
     tau1tau2 = jax.numpy.concatenate([tau1, tau2], axis=0)
     h2 = four_vec_sum(tau1tau2)
+    # in 4-momentum space
     return h2
 
 
@@ -327,6 +329,7 @@ def calculate_dihiggs_system(inputs):
     h1 = calculate_hbb(inputs)
     h2 = calculate_htautau(inputs)
     dihiggs_system = four_vec_sum(jax.numpy.concatenate([h1, h2], axis=0))
+    # in 4-momentum space
     return dihiggs_system
 
 
@@ -514,15 +517,17 @@ def calculate_dihiggs_system_phi(inputs):
 def calculate_cos_theta_h1(inputs):
     h1 = calculate_hbb(inputs)
     dihiggs_system = calculate_dihiggs_system(inputs)
-    h1_cms_dihiggs = boost_a_cm_of_b(jax.numpy.concatenate([h1, dihiggs_system], axis=0), dihiggs_system[3])
+    dihiggs_system_detspace = fourmomentum_to_det_coord(dihiggs_system)
+    h1_cms_dihiggs = boost_a_cm_of_b(jax.numpy.concatenate([h1, dihiggs_system], axis=0), dihiggs_system_detspace[3])
     cos_theta_h1 = signed_cos_deltaangle_for_jax(jax.numpy.concatenate([h1_cms_dihiggs[:3], dihiggs_system[:3]], axis=0))
     return cos_theta_h1
 
 
 def calculate_phi_h1(inputs):
-    h1 = calculate_hbb(inputs)
-    dihiggs_system = calculate_dihiggs_system(inputs)
-    h1_cms_dihiggs = boost_a_cm_of_b(jax.numpy.concatenate([h1, dihiggs_system], axis=0), dihiggs_system[3])
+    h1 = calculate_hbb(inputs)   # four momentum space
+    dihiggs_system = calculate_dihiggs_system(inputs)   # four momentum space
+    dihiggs_system_detspace = fourmomentum_to_det_coord(dihiggs_system)
+    h1_cms_dihiggs = boost_a_cm_of_b(jax.numpy.concatenate([h1, dihiggs_system], axis=0), dihiggs_system_detspace[3])
     h1_cms_dihiggs_detspace = fourmomentum_to_det_coord(h1_cms_dihiggs)
     return h1_cms_dihiggs_detspace[2]
 
@@ -865,7 +870,7 @@ def create_pdf_input_vars_reco_higgs(
     jac_matrix_constrained_space = jac_matrix[:, :, np.array([0, 1, 2, 5, 6, 8, 9, 10, 13, 14])]
     # squared_matrix = np.matmul(jac_matrix_transposed, jac_matrix)
     # squared_matrix = np.matmul(jac_matrix, jac_matrix_transposed)
-    jac_det = np.linalg.det(jac_matrix_constrained_space)
+    jac_det = abs(np.linalg.det(jac_matrix_constrained_space))
     jac_matrix_transposed = np.transpose(jac_matrix, axes=(0, 2, 1))
     squared_matrix = np.matmul(jac_matrix_transposed, jac_matrix)
     squared_matrix = np.matmul(jac_matrix, jac_matrix_transposed)
@@ -1041,8 +1046,8 @@ def create_pdf_input_vars_reco_top(
     jac_matrix_constrained_space = jac_matrix[:, :, np.array([0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14])]
     squared_matrix = np.matmul(jac_matrix_transposed, jac_matrix)
     squared_matrix = np.matmul(jac_matrix, jac_matrix_transposed)
-    jac_det = np.linalg.det(jac_matrix_constrained_space)
-    gramsche = np.linalg.det(squared_matrix)
+    jac_det = abs(np.linalg.det(jac_matrix_constrained_space))
+    gramsche = abs(np.linalg.det(squared_matrix))
 
     EMPTY_FLOAT = -99999.9
     pdf_input_vars_reco_top = ak.zip({